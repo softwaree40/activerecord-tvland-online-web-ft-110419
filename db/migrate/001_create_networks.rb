c285ed2bb1cde7fa63c107e8f0bfe8d94d0e54d5
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-01 22:20:05 +0000

    Done.

--- db/migrate/001_create_networks.rb
+++ db/migrate/001_create_networks.rb
@@ -5,6 +5,7 @@
 #001_create_networks.rb becomes class CreateNetworks
 class CreateNetworks < ActiveRecord::Migration[5.1]
   #define a change method in which to do the migration
+  
   def change
     create_table :networks do |t| #we get a block variable here for the table
       #primary key of :id is created for us!
