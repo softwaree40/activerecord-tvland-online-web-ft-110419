1ebb3d38f2ce6f14ebe53b0f42f3bc2ea02d8c18
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-02 02:20:45 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,8 @@ class Show < ActiveRecord::Base
       has_many :actors, :through => :characters
       
       belongs_to :network
+      
+      def actors_list
+        self.actors.map {|actor| "#{actor.first_name} #{actor.last_name}"}
+      end
 end
