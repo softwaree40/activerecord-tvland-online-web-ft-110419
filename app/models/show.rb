9c2464c8032941f08347cd75594cbcbf11a41c6a
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-02 01:41:33 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,8 @@ class Show < ActiveRecord::Base
       has_many :actors, :through => :characters
       
       belongs_to :network
+      def actors_list
+          self.actors
+          binding.pry
+      end
 end
